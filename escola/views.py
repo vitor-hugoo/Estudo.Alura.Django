fc1b581859381d82d7edec31bc22c135b6888781
vitor-hugoo <vitorhugobarros@hotmail.com>
2024-01-08 16:29:33 -0300

    finalização CRUD default Django

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,18 @@
 from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer, CursoSerializer
+
 
 # Create your views here.
 
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-def alunos(request):
-    if request.method == "GET":
-        aluno = {"id": 1, "nome": "Andrezes"}
-        return JsonResponse(aluno)
+
