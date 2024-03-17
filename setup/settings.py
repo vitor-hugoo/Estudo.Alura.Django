eea69a62aa1ddf46ecaa4a7fb3b0e7a877716541
vitor-hugoo <vitorhugobarros@hotmail.com>
2024-03-17 09:10:14 -0300

    limitar requicisões

--- setup/settings.py
+++ setup/settings.py
@@ -130,7 +130,14 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTEHNTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication'
-    ]
-
+    ],
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.AnonRateThrottle',
+    #     # 'rest_framework.throttling.UserRateThrottle',
+    # ],
+    # 'DEFAULT_THROTTLE_RATES':{
+    #     'anon': '5/day',
+    #     # 'user': '1000/day',
+    # },
 }
 
